ccabcc265cfc58853ae61afe319be3ded7dd8679
Diogo Regis <diogo.regis.silva@gmail.com>
2024-06-21 21:40:05 -0300

    configurando class User, atributos e dados para persistencia

--- src/main/java/br/com/diogo/cadBib/model/User.java
+++ src/main/java/br/com/diogo/cadBib/model/User.java
@@ -1,2 +1,28 @@
-package br.com.diogo.cadBib.model;public class User {
+package br.com.diogo.cadBib.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String role;
+
 }
