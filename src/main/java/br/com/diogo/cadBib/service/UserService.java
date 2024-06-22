a93cac403600952ccd8789403883025883485d14
Diogo Regis <diogo.regis.silva@gmail.com>
2024-06-21 22:40:31 -0300

    criando e configurando service UserService

---
+++ src/main/java/br/com/diogo/cadBib/service/UserService.java
@@ -0,0 +1,27 @@
+package br.com.diogo.cadBib.service;
+
+
+import br.com.diogo.cadBib.model.User;
+import br.com.diogo.cadBib.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User save(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+}
