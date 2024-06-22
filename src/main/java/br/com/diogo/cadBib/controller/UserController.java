60400c0f6b61a82713939634f7d83957f5ccdae3
Diogo Regis <diogo.regis.silva@gmail.com>
2024-06-21 22:51:13 -0300

    criando e configurando controller de User, UserController, com as rotas

---
+++ src/main/java/br/com/diogo/cadBib/controller/UserController.java
@@ -0,0 +1,42 @@
+package br.com.diogo.cadBib.controller;
+
+import br.com.diogo.cadBib.model.User;
+import br.com.diogo.cadBib.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model){
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(User user){
+        userService.save(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(){
+        return "user-dashboard";
+    }
+}
