81e18c719849a068343c770dfe507ec12b18b79a
Diogo Regis <diogo.regis.silva@gmail.com>
2024-06-21 23:47:17 -0300

    atualização do projeto para rodar teste, com criação das paginas e configuração

--- src/main/java/br/com/diogo/cadBib/service/UserService.java
+++ src/main/java/br/com/diogo/cadBib/service/UserService.java
@@ -1,27 +1,44 @@
 package br.com.diogo.cadBib.service;
 
-
 import br.com.diogo.cadBib.model.User;
 import br.com.diogo.cadBib.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    private UserRepository userRepository;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    public User save(User user){
+    public User save(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
-    public User findByUsername(String username){
-        return userRepository.findByUsername(username);
+    public UserDetails findByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return org.springframework.security.core.userdetails.User.withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return findByUsername(username);
     }
 }
